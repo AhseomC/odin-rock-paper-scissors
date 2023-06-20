d78a51cc38a0e3a874f7ede6a800db7f271c2d5f
AhseomC <109974962+AhseomC@users.noreply.github.com>
2023-06-20 16:35:00 +0900

    Add function that compares selection and return result

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ function getComputerChoice() {
 let playerAnswer = prompt("Rock? Paper? Scissors?");
 //Lowers the answer down to lower cases
 let playerSelection = playerAnswer.toLowerCase();
+let computerSelection = getComputerChoice();
 
+//Compares and returns result
+function compareSelections(playerSelection, computerSelection) {
+  let result;
+  if (playerSelection === computerSelection) {
+    result = 'Tie!';
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    result = 'You Lose! Paper beats Rock';
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    result = 'You Lose! Scissors beats Paper';
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    result = 'You Lose! Rock beats Scissors'
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    result = 'You Win! Paper beats Rock';
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    result = 'You Win! Scissors beats Paper';
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    result = 'You Win! Rock beats Scissors';
+  } 
+  return result;
+}
+console.log(compareSelections(playerSelection, computerSelection));
 
 
 
